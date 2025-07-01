f71059f25ad12e942c530ae4e038237549b88fa3
kj <kj>
2025-07-01 23:13:16 +0900

    オフィシャルグッズの告知を追加

--- pages/01_Information.py
+++ pages/01_Information.py
@@ -11,6 +11,26 @@ st.set_page_config(
 
 st.title("Information")
 
+st.write("---")
+st.subheader("オフィシャルグッズ受注販売（～ 7月11日(金) 23:59まで）")
+
+# ここにTwitterからコピーした特定のツイートの埋め込みコードを貼り付けます
+tweet_embed_code = """
+<blockquote class="twitter-tweet"><p lang="ja" dir="ltr">┈┈┈┈┈✧ 🎁 ✧┈┈┈┈<br>　 新オフィシャルグッズ<br>　 受注販売開始❕<br>┈┈┈┈┈┈┈┈┈┈┈┈┈<br>待望の初グッズがついに登場📣ˎˊ˗<br>この機会をお見逃しなく👀✧<a href="https://twitter.com/hashtag/%E5%A4%A9%E7%B5%86%E3%81%95%E3%81%95%E3%81%AF?src=hash&amp;ref_src=twsrc%5Etfw">#天絆ささは</a><a href="https://twitter.com/hashtag/%E5%B9%BD%E9%9F%B3%E3%81%97%E3%81%AE?src=hash&amp;ref_src=twsrc%5Etfw">#幽音しの</a><a href="https://twitter.com/hashtag/%E7%BE%BD%E6%B5%81%E9%B7%B2%E3%82%8A%E3%82%8A%E3%82%8A?src=hash&amp;ref_src=twsrc%5Etfw">#羽流鷲りりり</a><br><br>▼ ご購入はこちら<br>￤🛒<a href="https://t.co/fdcGLXffSD">https://t.co/fdcGLXffSD</a><br>￤📅 ～ 7月11日(金) 23:59まで <a href="https://t.co/jzPc4prw5B">pic.twitter.com/jzPc4prw5B</a></p>&mdash; ななしいんく公式🍩 (@774inc_official) <a href="https://twitter.com/774inc_official/status/1938561806253052194?ref_src=twsrc%5Etfw">June 27, 2025</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> """
+
+# 3つのカラムを作成：左の空白、中央のコンテンツ、右の空白
+col1, col2, col3 = st.columns([1, 2, 1])  # 割合を調整して中央のカラムの幅を決めます
+
+with col2:  # 中央のカラムにコンテンツを配置
+    if tweet_embed_code:
+        components.html(
+            tweet_embed_code,
+            height=800,
+            scrolling=True,
+        )
+    else:
+        st.info("Twitterの埋め込み情報が読み込まれませんでした。")
+
 # YouTube動画のURLを指定 (前回のコードからそのまま)
 youtube_url = "https://www.youtube.com/watch?v=LRowhAcHngc"
 
